664f042686e7d514645085f44cd56ec244bb9674
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-23 21:38:30 -0400

    update portfolio project

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>
+        Feel Free to Contact me
+      </p>
+    </div>
+  );
+}
