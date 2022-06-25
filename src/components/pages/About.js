069528b97afcf3cbdf112e79f7002f28c13628e9
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-25 14:36:07 -0400

    update About Page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,12 +8,25 @@ export default function About() {
       <h1 class="text-white body-title">About Page</h1>
       <div class="about-body">
       <p class="text-white">
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Welcome to my Portfolio Website.  I currently reside in the suburbs of Raleigh, NC.  I have
+        lived in the Raleigh area for approximately 15 years.  I am currently employeed with At&t with
+        a title of Manager of Outside Plant and Engineering.  I have been employeed for the company
+        since I graduated from Alfred State College, located in upstate New York.  
+    <br></br>
+    <br></br>
+        As a person in a very technical field, I am always looking for ways to improve my technical 
+        skill set. Front end web design has always been an interest, though I had never persued it 
+        as a career path.  My interest in front end design prompted me to research different learning
+        opportunities.  After a good deal of reasearch,  one of the most suggested paths to take was 
+        a Coding Bootcamp Course.  I then decided to sign up for University of North Carolina Charlotte 
+        Coding Bootcamp.  
+    <br></br>
+    <br></br>
+        This career path, while very difficult, is one of the most rewarding experiences I have had in 
+        my 15+ year career as an Engineer.  At the start of the Bootcamp,  I was not able to write a single
+        line of code.  Nearing the Tail end of the course,  I have now created 20+ functional applications
+        using many types of coding technologies.  I can honestly say after 6 months of classes,  I am now 
+        a Full Stack Developer.
       </p>
       </div>
     </div>
